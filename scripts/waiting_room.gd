be8f63e71dcea79018767e1bafab94026657862a
BrunoWilkinson <bruno.wilkinson@gmail.com>
2025-12-30 09:35:42 +0000

    refactor waiting room script

--- scripts/waiting_room.gd
+++ scripts/waiting_room.gd
@@ -1,16 +1,36 @@
 extends Control
 
+var start_game_button: Button = null
+var ready_button: Button = null
+var player_one_label: Label = null
+var player_two_label: Label = null
+var player_two_ready_label: Label = null
+
 func _ready() -> void:
+	start_game_button = $ActionContainer/StartGameButton
+	assert(start_game_button != null)
+	ready_button = $ActionContainer/ReadyButton
+	assert(ready_button != null)
+	player_one_label = $PlayerOneLabel
+	assert(player_one_label != null)
+	player_two_label = $PlayerTwoLabel
+	assert(player_two_label != null)
+	player_two_ready_label = $PlayerTwoReadyLabel
+	assert(player_two_ready_label != null)
+	
 	Lobby.player_connected.connect(_on_player_connected)
 	Lobby.player_disconnected.connect(_on_player_disconnected)
 	Lobby.server_disconnected.connect(_on_server_disconnected)
 	Lobby.player_list_changed.connect(_on_player_list_set)
 	Lobby.player_ready_state_changed.connect(_on_player_ready_state_changed)
-	$ActionContainer/StartGameButton.button_down.connect(_on_start_game_button_down)
-	$ActionContainer/StartGameButton.visible = multiplayer.is_server()
-	$ActionContainer/StartGameButton.disabled = !Lobby.can_start_game()
-	$ActionContainer/ReadyButton.button_down.connect(_on_ready_button_down)
-	$ActionContainer/ReadyButton.visible = !multiplayer.is_server()
+	
+	start_game_button.button_down.connect(_on_start_game_button_down)
+	start_game_button.visible = multiplayer.is_server()
+	start_game_button.disabled = !Lobby.can_start_game()
+	
+	ready_button.button_down.connect(_on_ready_button_down)
+	ready_button.visible = !multiplayer.is_server()
+	
 	_reset_ui()
 	_on_player_list_set()
 	
@@ -28,29 +48,29 @@ func _on_player_list_set() -> void:
 		_set_player_name(_get_player_label(player.get_peer_id()), player.get_username())
 
 func _on_player_ready_state_changed(_in_player_id: int, _is_ready: bool) -> void:
-	$ActionContainer/StartGameButton.disabled = !Lobby.can_start_game()
+	start_game_button.disabled = !Lobby.can_start_game()
 	
 	if Lobby.can_start_game():
-		$ActionContainer/ReadyButton.text = String("UnReady")
-		$PlayerTwoReadyLabel.visible = true
+		ready_button.text = String("UnReady")
+		player_two_ready_label.visible = true
 	else:
-		$ActionContainer/ReadyButton.text = String("Ready")
-		$PlayerTwoReadyLabel.visible = false
+		ready_button.text = String("Ready")
+		player_two_ready_label.visible = false
 
 func _set_player_name(label_node: Label, player_name: String) -> void:
 	label_node.show()
 	label_node.text = player_name
 
 func _reset_ui() -> void:
-	$PlayerOneLabel.hide()
-	$PlayerTwoLabel.hide()
-	$PlayerTwoReadyLabel.hide()
+	player_one_label.hide()
+	player_two_label.hide()
+	player_two_ready_label.hide()
 
 func _get_player_label(peer_id: int) -> Label:
 	if peer_id != 1:
-		return $PlayerTwoLabel
+		return player_two_label
 
-	return $PlayerOneLabel
+	return player_one_label
 
 func _on_start_game_button_down() -> void:
 	if !Lobby.can_start_game():
