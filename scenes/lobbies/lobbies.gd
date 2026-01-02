e743735f5c175cb852df44314ecbc43050075612
BrunoWilkinson <bruno.wilkinson@gmail.com>
2026-01-02 18:51:56 +0000

    fetch steam lobbies + add singleton init steam

--- scenes/lobbies/lobbies.gd
+++ scenes/lobbies/lobbies.gd
@@ -2,6 +2,9 @@ extends Control
 
 var _lobby_item_list: ItemList = null
 var _join_button: Button = null
+var _refresh_button: Button = null
+
+var _lobby_mapping: Dictionary[String, int] = {}
 
 func _ready() -> void:
 	_lobby_item_list = $ScrollContainer/LobbyList
@@ -9,13 +12,37 @@ func _ready() -> void:
 	
 	_join_button = $Join
 	assert(_join_button != null)
+	
+	_refresh_button = $Refresh
+	assert(_refresh_button != null)
 
-	for i in range(1, 50):
-		_lobby_item_list.add_item("Lobby " + str(i))
+	Steam.lobby_match_list.connect(_on_lobby_match_list)
+	Steam.addRequestLobbyListDistanceFilter(Steam.LOBBY_DISTANCE_FILTER_WORLDWIDE)
+	Steam.requestLobbyList()
+
 	_join_button.button_down.connect(_on_join_button_down)
+	_refresh_button.button_down.connect(_on_refresh_button_down)
 
 func _on_join_button_down() -> void:
 	if _lobby_item_list.is_anything_selected():
 		var idx = _lobby_item_list.get_selected_items()[0]
 		var text = _lobby_item_list.get_item_text(idx)
-		print(text)
+		Lobby.join_game(_lobby_mapping[text])
+
+func _on_lobby_match_list(lobbies: Array) -> void:
+	_lobby_mapping.clear()
+	_lobby_item_list.deselect_all()
+	_lobby_item_list.clear()
+
+	for index in lobbies.size():
+		var lobby_id = lobbies[index]
+		var lobby_name := Steam.getLobbyData(lobby_id, "name")
+		if lobby_name.is_empty():
+			lobby_name = "Lobby " + str(index + 1)
+		_lobby_mapping.get_or_add(lobby_name, lobby_id)
+		_lobby_item_list.add_item(lobby_name)
+
+	_lobby_item_list.select(0)
+
+func _on_refresh_button_down() -> void:
+	Steam.requestLobbyList()
---
+++ steam/steamworks.gd
@@ -0,0 +1,11 @@
+extends Node
+class_name SteamInit
+
+func _ready() -> void:
+	var initialize_response: Dictionary = Steam.steamInitEx(480, true)
+	print("Did Steam initialize?: %s " % initialize_response)
+	if initialize_response['status'] > Steam.STEAM_API_INIT_RESULT_OK:
+		print("Failed to initialize Steam, shutting down: %s" % initialize_response)
+
+func _process(_delta: float) -> void:
+	Steam.run_callbacks()
